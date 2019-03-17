25043bc19f0d2d3acb4f5272b19155a5a1705b48
Alice Liang <aliceliang011@gmail.com>
2019-03-17 18:24:53 -0400

    7.10 complete

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Alice Liang on 3/17/19.
+//  Copyright © 2019 Alice Liang. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
