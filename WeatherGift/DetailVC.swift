fa9e6af24ef17d729d6aca424d23967492563666
Alice Liang <aliceliang011@gmail.com>
2019-03-18 14:32:30 -0400

    7.17 complete

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -42,7 +42,9 @@ class DetailVC: UIViewController {
         locationLabel.text = locationsArray[currentPage].name
         dateLabel.text = locationsArray[currentPage].coordinates
         temperatureLabel.text = locationsArray[currentPage].currentTemp
-        print("%%% current Temp inside updateUserInterface = \(locationsArray[currentPage].currentTemp)")
+        summaryLabel.text = locationsArray[currentPage].currentSummary
+        currentImage.image = UIImage(named: locationsArray[currentPage].currentIcon)
+        
     }
 }
 
